6aead9e645a8d1de3a92adcc3e0f92eb12bc97f5
dziadyk <dziadyk_v@ukr.net>
2023-10-18 05:54:21 -0700

    add module

---
+++ employee_hide_work_permit/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'employee_hide_work_permit',
+    'summary': 'Employee hide work permit',
+    'description': 'Employee hide work permit',
+
+    'author': 'Dziadyk V',
+    'website': 'https://www.oneservice-consulting.com/',
+
+    'category': 'Human Resources/Employees',
+    'license': 'OPL-1',
+    'version': '16.0.1.0.0',
+
+    'depends': [
+        'base',
+        'Employees',
+    ],
+
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+    ],
+
+    'price': 0,
+    'currency': 'EUR',
+
+}
